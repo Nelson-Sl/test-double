b50ce660ec3221f21ad12e692f7f1cc482ef2a08
Shen Liao <shen.liao@thoughtworks.com>
2020-08-04 17:42:55 +0800

    Understand the concept of stub and mock

--- src/test/java/mock/SecurityCenterTest.java
+++ src/test/java/mock/SecurityCenterTest.java
@@ -1,19 +1,42 @@
 package mock;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class SecurityCenterTest {
     /* 需求描述：
-    编写SecurityCenter类的单元测试，单元测试switchOn方法，不依赖于DoorPanel的close的方法实现
+    编写SecurityCenter类的单元测试
+    原理：该需求需要确定switchOn()方法是否可以执行，其中又运行了底层的doorPanel.close()方法。因此可以mock doorPanel重写
+    close方法以确定其可以运行。
     * */
+
+    //Mock doorPanel -> 不使用doorPanels
+    class MockDoorPanel extends DoorPanel {
+        private boolean isCalled = false;
+
+        void close() {
+            isCalled = true;
+        }
+
+        boolean getCalled() {
+            return isCalled == true;
+        }
+    }
+
+    private MockDoorPanel mock;
+    private SecurityCenter securityCenter;
 
     @BeforeEach
     public void setUp() {
+        mock = new MockDoorPanel();
+        securityCenter = new SecurityCenter(mock);
     }
 
     @Test
     public void shouldVerifyDoorIsClosed() {
+        securityCenter.switchOn();
+        Assertions.assertEquals(true, mock.getCalled());
     }
 }
--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -1,15 +1,30 @@
 package stub;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class GradeServiceTest {
     /* 需求描述：
     编写GradeService类的单元测试，单元测试calculateAverageGrades方法
+    原理：由于calculateAverageGrades中使用了GradeSystem中的数据，因此在Mock测试的时候，需要重新继承GradeSystem类为
+    其定一个值以确认结果。
     * */
+    class MockGradeSystem extends GradeSystem {
+        public List<Double> gradesFor(long studentId) {
+            return Arrays.asList(80.0, 90.0, 100.0);
+        }
+    }
+
+    private MockGradeSystem newGrade = new MockGradeSystem();
 
     @Test
     public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
-        //Assertions.assertEquals(90.0, result);
+        GradeService gradeService = new GradeService(newGrade);
+        double result = gradeService.calculateAverageGrades(1);
+        Assertions.assertEquals(90.0, result);
     }
 }
